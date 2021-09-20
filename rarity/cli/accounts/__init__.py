f1d0bc8237e649fc0377b35cd523698941af1796
Bryan Stitt <bryan@stitthappens.com>
2021-09-19 18:05:04 -0700

    /name intead of argument

--- rarity/cli/accounts/__init__.py
+++ rarity/cli/accounts/__init__.py
@@ -9,17 +9,17 @@ def accounts():
 
 
 @accounts.command()
-@click.argument("account-name", type=str)
+@click.option("/name", type=str, prompt=True)
 @click.option("/mnemonic", prompt=True, hide_input=True)
 @click.option("/mnemonic-passphrase", prompt=True, default="", hide_input=True)
 @click.option("/encryption-passphrase", prompt=True, required=True, hide_input=True)
 @click.option("/offset", prompt=True, default=0, show_default=True)
 @click.option("/save-pass;/no-save-pass", default=False)
-def from_mnemonic(account_name, mnemonic, mnemonic_passphrase, encryption_passphrase, offset, save_pass):
+def from_mnemonic(name, mnemonic, mnemonic_passphrase, encryption_passphrase, offset, save_pass):
     """Create a brownie account from an existing mnemonic."""
     from .mnemonic import account_from_mnemonic
 
-    account_from_mnemonic(mnemonic, mnemonic_passphrase, offset, account_name, encryption_passphrase, save_pass=save_pass)
+    account_from_mnemonic(mnemonic, mnemonic_passphrase, offset, name, encryption_passphrase, save_pass=save_pass)
 
 
 # TODO: command to make a random account with a cool vanity address (but then we have to make sure backups are handled properly)
